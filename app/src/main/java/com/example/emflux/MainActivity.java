90fc07ef38d40c17bfb20f1c6e6d53fec506ffd4
Atif-Iqbal-08 <atifiqbal08@gmail.com>
2022-08-29 17:00:38 +0530

    made an update function with string arrays for messages

--- app/src/main/java/com/example/emflux/MainActivity.java
+++ app/src/main/java/com/example/emflux/MainActivity.java
@@ -11,6 +11,7 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.CompoundButton;
 import android.widget.FrameLayout;
+import android.widget.ImageButton;
 import android.widget.LinearLayout;
 import android.widget.NumberPicker;
 import android.widget.Switch;
@@ -22,10 +23,12 @@ import androidx.cardview.widget.CardView;
 import com.marcinmoskala.arcseekbar.ArcSeekBar;
 import com.marcinmoskala.arcseekbar.ProgressListener;
 
+import java.lang.reflect.Array;
+
 public class MainActivity extends Activity {
     SwitchCompat DirFwdRev , SmartSwitch;
-    TextView Forward ,Reverse, SmartOn ,SmartOff  , device_name;
-    Boolean open = false;
+    TextView Forward ,Reverse, SmartOn ,SmartOff  ,heading_status, direction_text_status, device_name;
+    Boolean open = false, power = false;
     ArcSeekBar gradientSeekBar;
     CardView cardView;
     //FrameLayout Running_Status_Frame;
@@ -33,9 +36,9 @@ public class MainActivity extends Activity {
     LinearLayout layout;
     TextView status;
     NumberPicker n1,n2,n3;
-    String[] time;
+    String[] time , Seeker_status , Running_Status;
     long animation_duration = 600;
-
+    ImageButton  power_toggle;
     Button Led_pop,Advanced_Menu;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -44,7 +47,9 @@ public class MainActivity extends Activity {
 
         gradientSeekBar = (ArcSeekBar) findViewById(R.id.gradientSeekBar);
         status =  (TextView)findViewById(R.id.status);
-
+        //for status array
+        Seeker_status = getResources().getStringArray(R.array.Seeker_Status);
+        Running_Status = getResources().getStringArray(R.array.Running_Status);
         //for slot view am /pm
         time = getResources().getStringArray(R.array.time);
         //popup for led initialization and class calling
@@ -71,65 +76,31 @@ public class MainActivity extends Activity {
 
 
         //Direction fwd reverse
-        DirFwdRev = (SwitchCompat) findViewById(R.id.switchOnOff);
+        DirFwdRev = (SwitchCompat) findViewById(R.id.Forward_Reverse_switch);
         Forward = findViewById(R.id.tvSwitchYes);
         Reverse = findViewById(R.id.tvSwitchNo);
 
-        //dir change function
-        DirFwdRev.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
-            @Override
-            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-                // TODO Auto-generated method stub
-                if (buttonView.isChecked()){
-                    Forward.setTextColor(Color.parseColor("#FF03DAC5"));
-                    Reverse.setTextColor(Color.parseColor("#FFFFFFFF"));
 
-                    //do something
-
-                    }
-
-                else {
-                     Forward.setTextColor(Color.parseColor("#FFFFFFFF"));
-                     Reverse.setTextColor(Color.parseColor("#FF03DAC5"));
-                        //do something
-
-                    }
-                }
-            });
         // Smart Change switch
             SmartSwitch = (SwitchCompat) findViewById(R.id.SmartOnOff);
             SmartOn = findViewById(R.id.tvSmartYes);
             SmartOff = findViewById(R.id.tvSmartNO);
-        SmartSwitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
-            @Override
-            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-                // TODO Auto-generated method stub
-                if (buttonView.isChecked()){
-                    SmartOff.setTextColor(Color.parseColor("#FF03DAC5"));
-                    SmartOn.setTextColor(Color.parseColor("#FFFFFFFF"));
-                    //do something
 
-                }
 
-                else {
-                    SmartOff.setTextColor(Color.parseColor("#FFFFFFFF"));
-                    SmartOn.setTextColor(Color.parseColor("#FF03DAC5"));
-                    //do something
+        // running status display
 
-                }
-            }
-        });
 
-// running status display
         //Running_Status_Frame = findViewById(R.id.Running_status_frame);
         Running_status = findViewById(R.id.Running_Status);
-        device_name =  findViewById(R.id.device_name);
+        direction_text_status = (TextView) findViewById(R.id.direction_text) ;
+       // device_name =  findViewById(R.id.device_name);
         device_heading = findViewById(R.id.device_heading);
 
         cardView = findViewById(R.id.card_view);
         linearLayout = findViewById(R.id.full_control);
         layout = findViewById(R.id.layout);
-
+        //power on off image button
+        power_toggle = (ImageButton) findViewById(R.id.power_button);
 
         //Number picker for timer
         n1 = findViewById(R.id.hours);
@@ -146,8 +117,9 @@ public class MainActivity extends Activity {
         n3.setMinValue(0);
         n3.setDisplayedValues(time);
         expand();
-
-
+        status();
+        //update status
+        heading_status = (TextView) findViewById(R.id.head_status);
 
         //gradient set
         int[] colorArrays = getResources().getIntArray(R.array.gradient);
@@ -156,22 +128,22 @@ public class MainActivity extends Activity {
 
 
         //set event
-        gradientSeekBar.setOnProgressChangedListener(new ProgressListener() {
-            @Override
-            public void invoke(int progress) {
-                if (progress != 7 ){
-                    Log.d("Value", "" +progress) ;
-                    status.setText(" "+progress+" ");}
-                else
-                {
-                 status.setText("Turbo");
-                }
-
-            }
-
-        });
-
-        }
+//        gradientSeekBar.setOnProgressChangedListener(new ProgressListener() {
+//            @Override
+//            public void invoke(int progress) {
+//                if (progress != 7 ){
+//                    Log.d("Value", "" +progress) ;
+//                    status.setText(" "+progress+" ");}
+//                else
+//                {
+//                 status.setText("Turbo");
+//                }
+//
+//            }
+//
+//        });
+//
+  }
 
 
     public void expand() {
@@ -181,7 +153,7 @@ public class MainActivity extends Activity {
             {
                 linearLayout.setVisibility(View.GONE);
                 Running_status.setVisibility(View.VISIBLE);
-                device_name.setTextSize(20);
+               // device_name.setTextSize(20);
 
                 open = false;
             }
@@ -189,10 +161,79 @@ public class MainActivity extends Activity {
             {
                 linearLayout.setVisibility(View.VISIBLE);
                 Running_status.setVisibility(View.GONE);
-                device_name.setTextSize(28);
+               // device_name.setTextSize(28);
                 open = true;
             }
         });
+
+
+
+
+
+    }
+    public void status() {
+        gradientSeekBar.setOnProgressChangedListener(new ProgressListener() {
+            @Override
+            public void invoke(int progress) {
+                Log.d("Value", "" +progress) ;
+                heading_status.setText(Running_Status[progress]);
+                status.setText(Seeker_status[progress]);
+            }
+
+
+
+        });
+        DirFwdRev.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                // TODO Auto-generated method stub
+                if (buttonView.isChecked()){
+                    Forward.setTextColor(Color.parseColor("#FF03DAC5"));
+                    Reverse.setTextColor(Color.parseColor("#FFFFFFFF"));
+                    direction_text_status.setText(Seeker_status[9]);
+                    heading_status.setText(Running_Status[9]);
+                    SmartSwitch.setChecked(false);
+                    //do something
+                }
+
+                else {
+                    Forward.setTextColor(Color.parseColor("#FFFFFFFF"));
+                    Reverse.setTextColor(Color.parseColor("#FF03DAC5"));
+                    direction_text_status.setText(Seeker_status[8]);
+
+                    //do something
+
+                }
+            }
+        });
+        SmartSwitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                // TODO Auto-generated method stub
+                if (buttonView.isChecked()){
+                    SmartOff.setTextColor(Color.parseColor("#FF03DAC5"));
+                    SmartOn.setTextColor(Color.parseColor("#FFFFFFFF"));
+                    heading_status.setText(Running_Status[8]);
+                    DirFwdRev.setChecked(false);
+                    status.setText((Seeker_status[10]));
+
+                    //do something
+                }
+
+                else {
+                    SmartOff.setTextColor(Color.parseColor("#FFFFFFFF"));
+                    SmartOn.setTextColor(Color.parseColor("#FF03DAC5"));
+
+                    //do something
+
+                }
+            }
+        });
+
+        power_toggle.setOnClickListener(view ->{
+
+        });
+
 
 
 
